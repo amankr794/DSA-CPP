c4379837dc2bbd440a355aa08dc89de7f1613a96
Amankr <109649639+amankr794@users.noreply.github.com>
2023-08-10 11:24:32 +0530

    basicOperator_typecasting.cpp
    This file contain Basic arithmetic operators and Typecasting code

---
+++ Basics of cpp/new.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    int b = 3;
+
+
+    /* cpp operators */
+    // cout << (a<=b) << endl;
+
+    cout << !(a==5);
+    
+
+    
+    // cout << (5)/((double)3);
+    
+    // int a = 5;
+    // int b = 3;
+
+    // cout << a+b << endl;
+    // cout << a-b << endl;
+    // cout << a*b << endl;
+    // cout << a%b << endl;
+    // cout << a/b << endl;
+
+
+    /*Typecasting*/
+    // double d = 5.7;
+
+    // int x = (int)d ;
+    // cout << "value of x is " << x << endl;
+    
+    // char ch = 97;
+    // cout << ch << endl;
+
+    // int num = 'b';
+    // cout << num << endl;
+    
+    // long long a = 5;
+    // cout << sizeof(a) << endl;
+    
+    // int a = 5;
+    // cout <<" value of a is : " << a << endl;
+
+    // char ch = 256;
+    // cout <<" value of ch is : " << ch << endl;
+
+    // bool flag = 1;
+    // cout << "value of flag is " << flag << endl;
+    
+    // cout <<  "Namaste Bharat\n" << endl ;
+    // cout << '\n';
+
+    // cout << "Enter a number: " << endl;
+
+    // int a;
+    // //taking input
+    // cin >> a;
+
+    // cout << "You entered " << a << endl;
+
+
+    float a=5.43,b=2.653;
+    float c=a/b;
+    cout<<c;
